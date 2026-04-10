be7047a20fbb82545349c85004f0b293e3636b28
AlphaBrate <moodlab.it@gmail.com>
2026-04-10 19:35:38 +0800

    fix: Update @updateURL and @downloadURL in user script header

--- dist/autoeb.user.js
+++ dist/autoeb.user.js
@@ -6,7 +6,8 @@
 // @author       ReTrn.
 // @match        https://lms1.wiseman.com.hk/lms/user/secure/course/eb/select_lesson/
 // @grant        none
-// @updateURL    
+// @updateURL    https://raw.githubusercontent.com/ChuTM/auto-eb/refs/heads/main/dist/autoeb.user.js
+// @downloadURL  https://raw.githubusercontent.com/ChuTM/auto-eb/refs/heads/main/dist/autoeb.user.js
 // ==/UserScript==
 (() => {
   var __async = (__this, __arguments, generator) => {
--- src/header.js
+++ src/header.js
@@ -6,6 +6,6 @@
 // @author       ReTrn.
 // @match        https://lms1.wiseman.com.hk/lms/user/secure/course/eb/select_lesson/
 // @grant        none
-// @updateURL    
-// @downloadURL  
+// @updateURL    https://raw.githubusercontent.com/ChuTM/auto-eb/refs/heads/main/dist/autoeb.user.js
+// @downloadURL  https://raw.githubusercontent.com/ChuTM/auto-eb/refs/heads/main/dist/autoeb.user.js
 // ==/UserScript==
