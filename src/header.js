446c51dbba8bd74f918eccf4d0dc2c5b3182a507
AlphaBrate <moodlab.it@gmail.com>
2026-04-12 22:27:33 +0800

    feat: add updater script and version environment file for version management

--- src/header.js
+++ src/header.js
@@ -7,6 +7,8 @@
 // @grant        GM_xmlhttpRequest
 // @require      https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/prism.min.js
 // @require      https://alb-cdn.web.app/popupjs/pu.min.js
+// @require      https://alb-cdn.web.app/popupjs/pu.min.js
+// @require      https://raw.githubusercontent.com/ChuTM/auto-eb/refs/heads/main/services/updater.user.js
 // @run-at       document-end
 // @match        https://*.wiseman.com.hk/*
 // @updateURL    https://raw.githubusercontent.com/ChuTM/auto-eb/refs/heads/main/dist/autoeb.user.js
