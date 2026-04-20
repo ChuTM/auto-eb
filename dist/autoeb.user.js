67bef1e118195b4cd6d669b7e38b5370fcdd7651
Chu Tsz Ming <131499354+ChuTM@users.noreply.github.com>
2026-04-20 18:20:17 +0800

    Update version number to 1.42

--- dist/autoeb.user.js
+++ dist/autoeb.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto EB
 // @namespace    http://tampermonkey.net/
-// @version      1.30
+// @version      1.42
 // @description  Complete your EB tasks in seconds.
 // @author       ReTrn.
 // @grant        GM_xmlhttpRequest
